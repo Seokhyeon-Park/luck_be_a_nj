e838153ad371314999f375f4f891b0f7a0bdbe9b
Seokbong <dfbgq346q@gmail.com>
2023-02-27 17:28:43 +0900

    symbol 돌아가는거 까지 만듬

--- lib/body/wheel/wheel.dart
+++ lib/body/wheel/wheel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './symbol/symbol.dart';
+import 'dart:math';
+import 'dart:async';
 
 class RouletteWheel extends StatefulWidget {
   const RouletteWheel({
@@ -16,6 +18,30 @@ class RouletteWheel extends StatefulWidget {
 }
 
 class _RouletteWheelState extends State<RouletteWheel> {
+  late final ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _scrollToBottom();
+  }
+
+  void _scrollToBottom() {
+    final random = Random();
+    final randomDouble = random.nextDouble() * 0.6 * 1000;
+
+    if(_controller.hasClients) {
+      Timer(Duration(milliseconds: randomDouble.toInt()), () {
+        _controller.animateTo(
+          _controller.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 800),
+          curve: Curves.easeOut,
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double wheelWidth = widget.width / 5 - 1.2;
@@ -36,12 +62,27 @@ class _RouletteWheelState extends State<RouletteWheel> {
           minScale: 1.0,
           maxScale: 1.0,
           child: SingleChildScrollView(
+            controller: _controller,
             scrollDirection: Axis.vertical,
             physics: const NeverScrollableScrollPhysics(),
             child: Column(
-              // mainAxisAlignment: MainAxisAlignment.end,
-              // mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
+                Symbol(symbolSize: wheelWidth),
                 Symbol(symbolSize: wheelWidth),
                 Symbol(symbolSize: wheelWidth),
                 Symbol(symbolSize: wheelWidth),
